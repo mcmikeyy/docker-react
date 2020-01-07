65ed2802a06c3d560b351fc3f2436b407813ea97
Michael Connors <michael.connors@gmail.com>
2020-01-07 16:07:25 +0000

    changed to develop

--- src/App.js
+++ src/App.js
@@ -7,8 +7,8 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload hi there again
+        <p className="App-intro">
+         this is change to develop
         </p>
         <a
           className="App-link"
